a70c8dddae84b20c0cf0d0115797c35c816c7dca
sinhpp <sinh.ern@student.passerellesnumeriques.org>
2025-03-15 12:16:31 +0700

    fail config

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -3,16 +3,12 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <title>POS System</title>
+    <title>K-WD Dashboard</title>
     <link
       href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;700;900&display=swap"
       rel="stylesheet"
     />
-    <link rel="stylesheet" href="../../views/assets/css/dashboard.css">
-    <link rel="stylesheet" href="../../views/assets/css/form.css">
-    <link rel="stylesheet" href="../../views/assets/css/style.css">
-    <link rel="stylesheet" href="../../views/assets/css/addpro.css">
-    <link rel="stylesheet" href="../../views/assets/css/form.forgot.password.css">
+    <link rel="stylesheet" href="./assets/css/dashboard.css">
     <script src="https://cdn.jsdelivr.net/gh/alpine-collective/alpine-magic-helpers@0.5.x/dist/component.min.js"></script>
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.3/dist/alpine.min.js" defer></script>
   </head>
