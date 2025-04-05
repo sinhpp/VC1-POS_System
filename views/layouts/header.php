1a0ea62bae5c6f6623fe6c79018a449ba93e2548
mesa <natmesa12a@gmail.com>
2025-04-06 00:14:13 +0700

    SCRUM-33: fix code create product

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -37,14 +37,6 @@
     <link rel="stylesheet" href="/views/assets/css/productCashier.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <script src="/views/assets/js/productCashier.js" defer></script>
-
-    <link rel="stylesheet" href="/views/assets/css/category.css">
-    <!-- In <head> -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-
-<!-- Before </body> -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-
 </head>
 
 <body>
